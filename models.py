79e1d7c85faf9794589f677e7f47d77d00d5fa82
hugueds <hugo-pdf@hotmail.com>
2021-07-25 17:30:26 -0300

    create the same function to get date and time as string

--- models.py
+++ models.py
@@ -2,8 +2,6 @@ import requests # request é a biblioteca responsavel por fazer requisições HT
 import json # É responsavel por padronizar entradas e saidas de dados no formato json
 import datetime
 
-
-
 def get_status_port(port,ip):  # Função criada para simplificar a requisição dos dados da remota
     remota_url = f'http://{ip}/api/crown/ac/GetRestDataOut' # Endereço reponsavel pela remota (api)
     param ={"data":{"Offset":port,"data_time":'hoje'}}  # indicação de qual porta eu quero pegar da remota ( comando json a ser enviado para a remota )
@@ -18,3 +16,9 @@ def get_time():
     year,month,day,hour,mili = now.strftime("%Y"),now.strftime("%m"),now.strftime("%d"),now.strftime("%X"),round(int(now.strftime("%f")),3)
     time_now = f"{year}-{month}-{day}T{hour}:{mili}Z"
     return time_now
+
+def get_time_v2():
+    now = datetime.datetime.now()
+    round_mili = round(now.microsecond, 3)
+    str_time = now.strftime('%Y-%m-%dT%'+round_mili)
+    return str_time
