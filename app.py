74610594507b5080940f598d75297839df5b7819
Hugo Peres <hugo-pdf@hotmail.com>
2021-08-07 19:21:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from decouple import config # acessa .env e seta as variaveis necessarias
 from models import get_status_port # acessar o arquivo de modelos e importar as funções necessarias
-import logging # importa bibiliote geradora de logs
+import logging # importa bibilioteca geradora de logs
 
 # adicionado o modulo de logging para ficar com a formatacao mais adequada
 logging.basicConfig(format="%(asctime)s %(message)s") # (Hugo)
@@ -8,8 +8,7 @@ logging.basicConfig(format="%(asctime)s %(message)s") # (Hugo)
 if __name__ == "__main__": 
     ip, port = config("ip_remota"), config("port") # pega ip e porta da remota do arquivo .env
     status = get_status_port(ip, port) # executar função e guardar o valor dentro da variavel
-    print(f"o status é : {status}") # print resultado
-    logging.info(f"O status eh {status}")
+    logging.info(f"O status  {status}") # exibe o resultado na tela
 
 
 
